12a37f98fb73dc68c8870333fcbafbc0142ae69c
BenJamin <41342911+BemjaminS@users.noreply.github.com>
2022-05-26 15:47:58 +0300

    fix bug

--- password-validator.sh
+++ password-validator.sh
@@ -20,30 +20,30 @@ FLAG=0
 if [ ${#PASSWORD} -ge 10 ]; then
     ((FLAG++))
 else
-    echo $ERROR_COLOR"Password most be at least 8 caracter"$RESET_COLOR
+    echo -e $ERROR_COLOR"Password most be at least 8 caracter"$RESET_COLOR
 fi
 #check if password contain lowwe case letter.
 if [[ $PASSWORD =~ [a-z] ]]; then
     ((FLAG++))
 else
-    echo $ERROR_COLOR"Password does not contain lowerCase latter"$RESET_COLOR
+    echo -e $ERROR_COLOR"Password does not contain lowerCase latter"$RESET_COLOR
 fi
 #check if paswword contain upper case letter.
 if [[ $PASSWORD =~ [A-Z] ]]; then
     ((FLAG++))
 else
-    echo $ERROR_COLOR"Password does not contain upperCase letter"$RESET_COLOR
+    echo -e $ERROR_COLOR"Password does not contain upperCase letter"$RESET_COLOR
 fi
 #check if passowrd contain number.
 if [[ $PASSWORD =~ [0-9] ]]; then
     ((FLAG++))
 else
-    echo $ERROR_COLOR"Password does not contain Number" $RESET_COLOR
+    echo -e $ERROR_COLOR"Password does not contain Number" $RESET_COLOR
 fi
 
 #Checks if the password has passed all checks.
 if [ $FLAG -eq 4 ]; then
-    echo $VALID_COLOR"Password vailid - Strong Password"$RESET_COLOR
+    echo -e $VALID_COLOR"Password vailid - Strong Password"$RESET_COLOR
     echo $?
     exit 0
 else
